16e7669c8da393376d42670487505a8feda03df4
Samuel Thomas <sgpthomas@gmail.com>
2017-08-03 22:31:41 +0000

    apparently setuptools only supports one author

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = "pydsef",
     version = "0.1.2",
-    author = ["Samuel Thomas", "Yousif Aldolaijan"],
+    author = "Samuel Thomas",
     author_email = "sgt43@cornell.edu",
     description = ("A python library that provides convience functions for DSEF"),
     packages = ['pydsef'],
