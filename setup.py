d4b1a912608a65ffba3b0ae6f9030a2c2fa715b4
Samuel Thomas <sgpthomas@gmail.com>
2017-08-03 22:06:59 +0000

    version bump to 0.1.2 for publishing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "pydsef",
-    version = "0.1.1",
+    version = "0.1.2",
     author = ["Samuel Thomas", "Yousif Aldolaijan"],
     author_email = "sgt43@cornell.edu",
     description = ("A python library that provides convience functions for DSEF"),
